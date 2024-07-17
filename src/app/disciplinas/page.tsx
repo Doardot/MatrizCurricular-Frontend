9af0572aef974925981a3836e76b206ff1a4044c
Eduardo Tavares <eduardo.tavares002@edu.pucrs.br>
2024-07-17 10:55:22 -0300

    Fix semestreContainer

--- src/app/disciplinas/page.tsx
+++ src/app/disciplinas/page.tsx
@@ -1,14 +1,18 @@
+"use client"
+
 import Header from '@/components/Header';
-import DisciplinaCard from "@/components/DisciplinaCard";
 import './styles.css';
-
-
-
 import React from 'react';
+import SemesterContainer from '@/components/SemestreContainer';
 
 export default function Disciplinas() {
 
     const disciplinaExemplo = {
+        nome: 'Laboratorio de Redes Computadores',
+        codCred: 'MAT101',
+        semestre: '2022.1'
+    };
+    const disciplinaExemplo2 = {
         nome: 'Laboratorio de Redes Computadores',
         codCred: 'MAT101',
         semestre: '2022.1'
@@ -20,7 +24,7 @@ export default function Disciplinas() {
             <div className="page">
 
                 <div className="cardSemester">
-                    <DisciplinaCard disciplina={disciplinaExemplo} />
+                    <SemesterContainer disciplinas={[disciplinaExemplo, disciplinaExemplo2]} onSave={() => { }} />
                 </div>
 
 
--- src/components/SemestreContainer/index.tsx
+++ src/components/SemestreContainer/index.tsx
@@ -1,9 +1,29 @@
+"use client"
+
 import React from 'react';
 import './index.css';
+import DisciplinaCard from '../DisciplinaCard';
 
-export default function SemesterContainer() {
+interface SemesterContainer {
+    disciplinas: any[];
+    onSave: () => void;
+}
+
+export default function SemesterContainer({
+    disciplinas,
+    onSave
+}: SemesterContainer) {
     return (
-        <div className="cardSemestre">
+        <div>
+            <div className='semesterContainer'>
+                {disciplinas.map((disciplina, index) => (
+                    <DisciplinaCard key={index} disciplina={disciplina} />
+                ))}
+                <div>
+                    {/* Void box for creating new disciplines */}
+                    <button onClick={onSave}>Save</button>
+                </div>
+            </div>
         </div>
     );
 }
